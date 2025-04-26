5a8f8a91a54c4dbed56c85309cc9bc040fbba28a
Prabath Samarasinghe <prabhaths2001@gmail.com>
2025-04-26 21:06:10 +0530

    Delete button added

---
+++ my-next-app/src/app/api/customer/delete/route.ts
@@ -0,0 +1,11 @@
+import { deleteCustomer } from "@/lib/controllers/customerControllers/Customer.controllers";
+
+export async function POST(request: Request) {
+    const { customerId } = await request.json();
+    try {
+        const customer = await deleteCustomer(customerId);
+        return new Response(JSON.stringify(customer), { status: 200 });
+    } catch (error) {
+        return new Response("Error deleting customer", { status: 500 });
+    }
+}
