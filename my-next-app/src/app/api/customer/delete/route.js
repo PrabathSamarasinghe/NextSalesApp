570a877b8f013fae4e0d9b006b9c0a46ca3ed4b9
Prabath Samarasinghe <prabhaths2001@gmail.com>
2025-04-26 21:09:09 +0530

    error created

--- my-next-app/src/app/api/customer/delete/route.js
+++ my-next-app/src/app/api/customer/delete/route.js
@@ -1,11 +1,11 @@
 import { deleteCustomer } from "@/lib/controllers/customerControllers/Customer.controllers";
 
-export async function POST(request: Request) {
+export async function POST(request) {
     const { customerId } = await request.json();
     try {
         const customer = await deleteCustomer(customerId);
         return new Response(JSON.stringify(customer), { status: 200 });
     } catch (error) {
-        return new Response("Error deleting customer", { status: 500 });
+        return new Response("Error deleting customer", { status: 500 , statusText: error.message});
     }
 }
