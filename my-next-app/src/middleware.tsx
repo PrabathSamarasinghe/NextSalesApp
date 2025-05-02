5ab6392d085fa5185abcd3a4bf7e1605530a5bcb
Prabath Samarasinghe <prabhaths2001@gmail.com>
2025-05-02 09:06:22 +0530

    Update middleware.tsx

--- my-next-app/src/middleware.tsx
+++ my-next-app/src/middleware.tsx
@@ -19,19 +19,6 @@ export async function middleware(request: NextRequest) {
 // This is the critical part - make sure your matcher includes ALL protected paths
 export const config = {
   matcher: [
-    '/dashboard', 
-    '/dashboard/:path*',
-    '/customers', 
-    '/customers/:path*',
-    '/invoices', 
-    '/invoices/:path*',
-    '/newInvoices', 
-    '/newInvoices/:path*',
-    '/products', 
-    '/products/:path*',
-    '/sales-report', 
-    '/sales-report/:path*',
-    '/customerdata/:path*',
-    '/newInvoice'
+    '/:path*',
   ],
 };
