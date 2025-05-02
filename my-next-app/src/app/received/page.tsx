f6e248d678ae6d2eaa38fea972debe579c227da8
Prabath Samarasinghe <prabhaths2001@gmail.com>
2025-05-02 18:12:13 +0530

    Update page.tsx

--- my-next-app/src/app/received/page.tsx
+++ my-next-app/src/app/received/page.tsx
@@ -161,7 +161,7 @@ export default function ReceivedInvoicesList() {
         <div className="flex space-x-3">
           <Link
             href="/recieved-invoice"
-            className="inline-flex items-center px-4 py-2 bg-blue-50 text-blue-600 font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200 shadow-sm"
+            className="inline-flex items-center px-4 py-2 bg-blue-50 text-blue-600 font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200 shadow-sm"
           >
             New Received Invoice
           </Link>
